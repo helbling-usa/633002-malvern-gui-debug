9dee3a27d8d4eac0bfabc0270efa96b089c62804
REF-helbling <reza.fard@helbling-inc.com>
2024-02-13 10:30:21 -0800

    added version number to title bar

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1444,7 +1444,7 @@ class GUI:
 
     def set_main_window(self, root):
         # root.geometry("1200x800+50+50")
-        root.title("DEBUG / MANUAL MODE GUI")
+        root.title("DEBUG / MANUAL MODE GUI (v 1.0)")
         root.resizable(False, False)
         # root.overrideredirect(True)
         window_height = 800
